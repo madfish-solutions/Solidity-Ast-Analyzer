0c858530ec9b663e2d66cf5f7bd77e008d5ac11c
kstasi <nastasiakondaurova@gmail.com>
2019-12-11 12:47:46 +0200

    statistics based on 43 contratcs

---
+++ contracts/Roles.sol
@@ -0,0 +1,292 @@
+pragma solidity ^0.5.0;
+
+/**
+ * @title Roles
+ * @dev Library for managing addresses assigned to a Role.
+ */
+library Roles {
+    struct Role {
+        mapping (address => bool) bearer;
+    }
+
+    /**
+     * @dev Give an account access to this role.
+     */
+    function add(Role storage role, address account) internal {
+        require(!has(role, account), "Roles: account already has role");
+        role.bearer[account] = true;
+    }
+
+    /**
+     * @dev Remove an account's access to this role.
+     */
+    function remove(Role storage role, address account) internal {
+        require(has(role, account), "Roles: account does not have role");
+        role.bearer[account] = false;
+    }
+
+    /**
+     * @dev Check if an account has this role.
+     * @return bool
+     */
+    function has(Role storage role, address account) internal view returns (bool) {
+        require(account != address(0), "Roles: account is the zero address");
+        return role.bearer[account];
+    }
+}
+
+contract Context {
+    // Empty internal constructor, to prevent people from mistakenly deploying
+    // an instance of this contract, which should be used via inheritance.
+    constructor () internal { }
+    // solhint-disable-previous-line no-empty-blocks
+
+    function _msgSender() internal view returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+contract CapperRole is Context {
+    using Roles for Roles.Role;
+
+    event CapperAdded(address indexed account);
+    event CapperRemoved(address indexed account);
+
+    Roles.Role private _cappers;
+
+    constructor () internal {
+        _addCapper(_msgSender());
+    }
+
+    modifier onlyCapper() {
+        require(isCapper(_msgSender()), "CapperRole: caller does not have the Capper role");
+        _;
+    }
+
+    function isCapper(address account) public view returns (bool) {
+        return _cappers.has(account);
+    }
+
+    function addCapper(address account) public onlyCapper {
+        _addCapper(account);
+    }
+
+    function renounceCapper() public {
+        _removeCapper(_msgSender());
+    }
+
+    function _addCapper(address account) internal {
+        _cappers.add(account);
+        emit CapperAdded(account);
+    }
+
+    function _removeCapper(address account) internal {
+        _cappers.remove(account);
+        emit CapperRemoved(account);
+    }
+}
+
+contract MinterRole is Context {
+    using Roles for Roles.Role;
+
+    event MinterAdded(address indexed account);
+    event MinterRemoved(address indexed account);
+
+    Roles.Role private _minters;
+
+    constructor () internal {
+        _addMinter(_msgSender());
+    }
+
+    modifier onlyMinter() {
+        require(isMinter(_msgSender()), "MinterRole: caller does not have the Minter role");
+        _;
+    }
+
+    function isMinter(address account) public view returns (bool) {
+        return _minters.has(account);
+    }
+
+    function addMinter(address account) public onlyMinter {
+        _addMinter(account);
+    }
+
+    function renounceMinter() public {
+        _removeMinter(_msgSender());
+    }
+
+    function _addMinter(address account) internal {
+        _minters.add(account);
+        emit MinterAdded(account);
+    }
+
+    function _removeMinter(address account) internal {
+        _minters.remove(account);
+        emit MinterRemoved(account);
+    }
+}
+
+contract PauserRole is Context {
+    using Roles for Roles.Role;
+
+    event PauserAdded(address indexed account);
+    event PauserRemoved(address indexed account);
+
+    Roles.Role private _pausers;
+
+    constructor () internal {
+        _addPauser(_msgSender());
+    }
+
+    modifier onlyPauser() {
+        require(isPauser(_msgSender()), "PauserRole: caller does not have the Pauser role");
+        _;
+    }
+
+    function isPauser(address account) public view returns (bool) {
+        return _pausers.has(account);
+    }
+
+    function addPauser(address account) public onlyPauser {
+        _addPauser(account);
+    }
+
+    function renouncePauser() public {
+        _removePauser(_msgSender());
+    }
+
+    function _addPauser(address account) internal {
+        _pausers.add(account);
+        emit PauserAdded(account);
+    }
+
+    function _removePauser(address account) internal {
+        _pausers.remove(account);
+        emit PauserRemoved(account);
+    }
+}
+
+contract SignerRole is Context {
+    using Roles for Roles.Role;
+
+    event SignerAdded(address indexed account);
+    event SignerRemoved(address indexed account);
+
+    Roles.Role private _signers;
+
+    constructor () internal {
+        _addSigner(_msgSender());
+    }
+
+    modifier onlySigner() {
+        require(isSigner(_msgSender()), "SignerRole: caller does not have the Signer role");
+        _;
+    }
+
+    function isSigner(address account) public view returns (bool) {
+        return _signers.has(account);
+    }
+
+    function addSigner(address account) public onlySigner {
+        _addSigner(account);
+    }
+
+    function renounceSigner() public {
+        _removeSigner(_msgSender());
+    }
+
+    function _addSigner(address account) internal {
+        _signers.add(account);
+        emit SignerAdded(account);
+    }
+
+    function _removeSigner(address account) internal {
+        _signers.remove(account);
+        emit SignerRemoved(account);
+    }
+}
+
+contract WhitelistAdminRole is Context {
+    using Roles for Roles.Role;
+
+    event WhitelistAdminAdded(address indexed account);
+    event WhitelistAdminRemoved(address indexed account);
+
+    Roles.Role private _whitelistAdmins;
+
+    constructor () internal {
+        _addWhitelistAdmin(_msgSender());
+    }
+
+    modifier onlyWhitelistAdmin() {
+        require(isWhitelistAdmin(_msgSender()), "WhitelistAdminRole: caller does not have the WhitelistAdmin role");
+        _;
+    }
+
+    function isWhitelistAdmin(address account) public view returns (bool) {
+        return _whitelistAdmins.has(account);
+    }
+
+    function addWhitelistAdmin(address account) public onlyWhitelistAdmin {
+        _addWhitelistAdmin(account);
+    }
+
+    function renounceWhitelistAdmin() public {
+        _removeWhitelistAdmin(_msgSender());
+    }
+
+    function _addWhitelistAdmin(address account) internal {
+        _whitelistAdmins.add(account);
+        emit WhitelistAdminAdded(account);
+    }
+
+    function _removeWhitelistAdmin(address account) internal {
+        _whitelistAdmins.remove(account);
+        emit WhitelistAdminRemoved(account);
+    }
+}
+
+contract WhitelistedRole is Context, WhitelistAdminRole {
+    using Roles for Roles.Role;
+
+    event WhitelistedAdded(address indexed account);
+    event WhitelistedRemoved(address indexed account);
+
+    Roles.Role private _whitelisteds;
+
+    modifier onlyWhitelisted() {
+        require(isWhitelisted(_msgSender()), "WhitelistedRole: caller does not have the Whitelisted role");
+        _;
+    }
+
+    function isWhitelisted(address account) public view returns (bool) {
+        return _whitelisteds.has(account);
+    }
+
+    function addWhitelisted(address account) public onlyWhitelistAdmin {
+        _addWhitelisted(account);
+    }
+
+    function removeWhitelisted(address account) public onlyWhitelistAdmin {
+        _removeWhitelisted(account);
+    }
+
+    function renounceWhitelisted() public {
+        _removeWhitelisted(_msgSender());
+    }
+
+    function _addWhitelisted(address account) internal {
+        _whitelisteds.add(account);
+        emit WhitelistedAdded(account);
+    }
+
+    function _removeWhitelisted(address account) internal {
+        _whitelisteds.remove(account);
+        emit WhitelistedRemoved(account);
+    }
+}
---
+++ contracts/Secondary.sol
@@ -0,0 +1,64 @@
+pragma solidity ^0.5.0;
+
+contract Context {
+    // Empty internal constructor, to prevent people from mistakenly deploying
+    // an instance of this contract, which should be used via inheritance.
+    constructor () internal { }
+    // solhint-disable-previous-line no-empty-blocks
+
+    function _msgSender() internal view returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+/**
+ * @dev A Secondary contract can only be used by its primary account (the one that created it).
+ */
+contract Secondary is Context {
+    address private _primary;
+
+    /**
+     * @dev Emitted when the primary contract changes.
+     */
+    event PrimaryTransferred(
+        address recipient
+    );
+
+    /**
+     * @dev Sets the primary account to the one that is creating the Secondary contract.
+     */
+    constructor () internal {
+        address msgSender = _msgSender();
+        _primary = msgSender;
+        emit PrimaryTransferred(msgSender);
+    }
+
+    /**
+     * @dev Reverts if called from any account other than the primary.
+     */
+    modifier onlyPrimary() {
+        require(_msgSender() == _primary, "Secondary: caller is not the primary account");
+        _;
+    }
+
+    /**
+     * @return the address of the primary.
+     */
+    function primary() public view returns (address) {
+        return _primary;
+    }
+
+    /**
+     * @dev Transfers contract to a new primary.
+     * @param recipient The address of new primary.
+     */
+    function transferPrimary(address recipient) public onlyPrimary {
+        require(recipient != address(0), "Secondary: new primary is the zero address");
+        _primary = recipient;
+        emit PrimaryTransferred(recipient);
+    }
+}
